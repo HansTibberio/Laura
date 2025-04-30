68653356190cb7b5f1a59dd8ec8065b471bfd8a0
HansTibberio <hanstiberio@proton.me>
2025-04-30 17:41:27 -0600

    Update pv display

--- laura_engine/src/thread.rs
+++ laura_engine/src/thread.rs
@@ -10,26 +10,20 @@ use std::{
 use laura_core::{legal_moves, Move, MoveList};
 
 use crate::{
-    config::{INFINITY, MATE, MAX_DEPTH, MAX_MATE},
+    config::{MATE, MAX_MATE},
     position::Position,
     search::{MainThread, PrincipalVariation, WorkerThread},
     timer::TimeControl,
     TimeManager,
 };
 
-#[derive(Debug, Clone, Copy, Default)]
-pub struct SearchStack {
-    pub score: i32,
-    pub best_move: Option<Move>,
-}
-
 #[derive(Debug)]
 pub struct Thread {
-    pub search_stack: [SearchStack; MAX_DEPTH],
     pub time_manager: TimeManager,
     pub principal_variation: PrincipalVariation,
     pub nodes: u64,
     pub ply: usize,
+    pub seldepth: usize,
     pub score: i32,
     pub depth: usize,
 }
@@ -53,38 +47,40 @@ impl Display for Thread {
         let nps: u128 = (nodes as u128 * 1000) / time;
         write!(
             f,
-            "info time {} score {} depth {} nodes {} nps {} {}",
-            time, score, self.depth, nodes, nps, self.principal_variation
-        )
+            "info score {} depth {} seldepth {} time {} nodes {} nps {} {}",
+            score, self.depth, self.seldepth, time, nodes, nps, self.principal_variation
+        )?;
+        Ok(())
     }
 }
 
 impl Thread {
     pub fn new(time_manager: TimeManager) -> Self {
         Self {
-            search_stack: [SearchStack::default(); MAX_DEPTH],
             principal_variation: PrincipalVariation::default(),
             nodes: 0,
             ply: 0,
-            score: -INFINITY,
+            seldepth: 0,
+            score: 0,
             depth: 0,
             time_manager,
         }
     }
 
     pub fn spinner(stop: Arc<AtomicBool>, nodes: Arc<AtomicU64>) -> Self {
-        Self::new(TimeManager::spinner(stop, nodes))
+        Self::new(TimeManager::new(stop, nodes, TimeControl::Infinite, false))
     }
 
     pub fn best_move(&self) -> Move {
-        self.principal_variation.best_move()
+        self.principal_variation.moves[0]
     }
 
     pub fn set_up(&mut self) {
         self.principal_variation = PrincipalVariation::default();
         self.nodes = 0;
         self.ply = 0;
-        self.score = -INFINITY;
+        self.seldepth = 0;
+        self.score = 0;
         self.depth = 0;
     }
 }
@@ -139,15 +135,15 @@ impl ThreadPool {
         let moves: MoveList = legal_moves!(&position.board());
         if moves.is_empty() {
             if position.in_check() {
-                println!("info time 0 score mate 0 depth 0");
+                println!("info score mate 0 depth 0 time 0");
             } else {
-                println!("info time 0 score cp 0 depth 0");
+                println!("info score cp 0 depth 0 time 0");
             }
             return None;
         }
 
         if moves.len() == 1 || self.main.time_manager.not_search() {
-            return Some(moves.moves()[0]);
+            return Some(moves[0]);
         }
 
         self.set_up();
