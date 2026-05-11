7356eab67730e80fa0b163bd7be8b0b6aa504329
HansTibberio <hanstiberio@proton.me>
2026-05-11 17:22:47 -0600

    History table inlining

--- src/tables.rs
+++ src/tables.rs
@@ -23,7 +23,9 @@
 
 use crate::config::{KILLER_SLOTS, MAX_PLY};
 use laura_core::{Color, Move};
+use crate::config::{HIST_CLAMP, KILLER_SLOTS, MAX_PLY};
 
+// Killer Moves
 #[derive(Debug)]
 pub struct KillerMoves {
     table: [[Option<Move>; KILLER_SLOTS]; MAX_PLY],
@@ -38,6 +40,7 @@ impl Default for KillerMoves {
 }
 
 impl KillerMoves {
+    #[inline(always)]
     pub fn store(&mut self, ply: usize, mv: Move) {
         if self.table[ply][0] != Some(mv) {
             self.table[ply][1] = self.table[ply][0];
@@ -45,11 +48,13 @@ impl KillerMoves {
         }
     }
 
+    #[inline(always)]
     pub fn get(&self, ply: usize) -> [Option<Move>; KILLER_SLOTS] {
         self.table[ply]
     }
 }
 
+// History Table
 #[derive(Debug, Copy, Clone)]
 pub struct HistoryTable {
     // [side_to_move][from_square][to_square]
@@ -66,20 +71,23 @@ impl Default for HistoryTable {
 
 impl HistoryTable {
     /// Updates de move that causes the beta cutoff
+    #[inline(always)]
     pub fn update_cutoff(&mut self, mv: Move, depth: usize, color: Color) {
-        let bonus: i32 = self.calculate_bonus(depth as i32);
+        let bonus: i32 = calculate_bonus(depth as i32);
         self.add_score(mv, color, bonus);
     }
 
     /// Penalizes the moves that were searched but didn't cause a beta cutoff
+    #[inline(always)]
     pub fn update_non_cutoffs(&mut self, quiets: &[Move], depth: usize, color: Color) {
-        let penalty: i32 = -self.calculate_bonus(depth as i32);
+        let penalty: i32 = -calculate_bonus(depth as i32);
         for &mv in quiets {
             self.add_score(mv, color, penalty);
         }
     }
 
     /// Gets the score of a Move
+    #[inline(always)]
     pub fn get_score(&self, mv: Move, color: Color) -> i32 {
         let color_idx: usize = color as usize;
         let from: usize = mv.get_src() as usize;
@@ -89,6 +97,7 @@ impl HistoryTable {
     }
 
     /// Adds a score (bonus or penalties) to a move
+    #[inline(always)]
     fn add_score(&mut self, mv: Move, color: Color, delta: i32) {
         let color_idx: usize = color as usize;
         let from: usize = mv.get_src() as usize;
@@ -96,6 +105,12 @@ impl HistoryTable {
 
         let old_score: i32 = self.table[color_idx][from][to];
         let new_score: i32 = old_score + delta - (old_score * delta.abs()) / 16384;
+        let new_score: i32 = old_score + delta - (old_score * delta.abs()) / HIST_CLAMP;
+
+        self.table[color_idx][from][to] = new_score.clamp(-HIST_CLAMP, HIST_CLAMP);
+    }
+}
+
 
         self.table[color_idx][from][to] = new_score.clamp(-16384, 16384);
     }
@@ -104,4 +119,8 @@ impl HistoryTable {
         let depth: i32 = depth.min(12);
         depth * depth + 2 * depth - 2
     }
+#[inline(always)]
+fn calculate_bonus(depth: i32) -> i32 {
+    let depth: i32 = depth.min(12);
+    depth * depth + 2 * depth - 2
 }
